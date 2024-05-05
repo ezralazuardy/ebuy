d67151c04998151f74abe3b06012a73ee4e4d050
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-05-05 16:29:33 +0700

    feat: update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/app/globals.css";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
